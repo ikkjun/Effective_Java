d8b5c9d5afbdc9763b822913a2020cae7cad7d99
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-04 23:59:42 +0900

    EffectiveJava

--- chapter2/Item2/BuilderPattern/NutritionFacts.java
+++ chapter2/Item2/BuilderPattern/NutritionFacts.java
@@ -5,4 +5,34 @@ public class NutritionFacts {
     private final int fat;
     private final int sodium;
     private final int carbonbydrate;
+
+    public static class Builder {
+        // 필수 매개변수
+        private final int servingSize;
+        private final int servings;
+
+        // 선택 매개변수 - 기본값으로 초기화한다.
+        private final int calories = 0;
+        private final int fat = 0;
+        private final int sodium = 0;
+        private final int carbonbydrate = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val)
+            {calories = val;    return this;}
+        public Builder fat(int val)
+            {fat = val;    return this;}
+        public Builder sodium(int val)
+            {sodium = val;    return this;}
+        public Builder carbohydrate(int val)
+            {carbohydrate = val;    return this;}
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
 }
