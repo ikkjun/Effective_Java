ec1e32c801de611fa5ef3bdd9fbe7fbec0c749aa
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-04 21:32:53 +0900

    study

--- chapter2/Item2/BuilderPattern/NutritionFacts.java
+++ chapter2/Item2/BuilderPattern/NutritionFacts.java
@@ -1,3 +1,8 @@
 public class NutritionFacts {
-
+    private final int servingSize; // (ml, 1회 제공량)  필수
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbonbydrate;
 }
