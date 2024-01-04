2d4ff1227db5f215db6592e90b5463410be40cca
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-04 20:10:35 +0900

    study

---
+++ chapter2/src/JavaBeans_pattern.java
@@ -0,0 +1,16 @@
+public class JavaBeans_pattern {
+    private int servingSize = -1;   // 필수
+    private int servings = -1;      // 필수
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public JavaBeans_pattern() {}
+    public void setServingSize(int val) {servingSize = val;}
+    public void setServings(int val) {servings = val;}
+    public void setCalories(int val) {calories = val;}
+    public void setFat(int val) {fat = val;}
+    public void setSodium(int val) {sodium = val;}
+    public void setCarbohydrate(int val) {carbohydrate = val;}
+}
