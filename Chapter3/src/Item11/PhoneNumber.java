e43ef52f5de3655638728b84602119da29d1d15b
ikkjun <89628722+ikkjun@users.noreply.github.com>
2024-01-18 10:56:03 +0900

    Effective Java

--- Chapter3/src/Item11/PhoneNumber.java
+++ Chapter3/src/Item11/PhoneNumber.java
@@ -52,4 +52,20 @@ public final class PhoneNumber {
 //        }
 //        return result;
 //    }
+
+    /**
+     * 이 전화번호의 문자열을 반환한다.
+     * 이 문자열은 "XXX-YYY-ZZZZ" 형태의 12글자로 구성된다.
+     * XXX는 지역코드, YYY는 프리픽스, ZZZZ는 가입자 번호다.
+     * 각각의 대문자는 10진수 숫자 하나를 나타낸다.
+     *
+     * 전화번호의 각 부분의 값이 너무 작아서 자릿수를 채운 수 없다면,
+     * 앞에서부터 0으로 채워나간다. 예컨대 가입자 번호가 123이라면
+     * 전화번호의 마지막 네 문자는 "123"이 된다.
+     */
+    @Override public String toString() {
+        return String.format("%03d-%03d-%04d", areaCode, prefix, lineNum);
+    }
+
+
 }
